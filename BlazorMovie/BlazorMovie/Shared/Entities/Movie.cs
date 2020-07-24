7a10038063a90d16120bf238e9e372f5eb850025
Tanner Percival <percivaltanner@gmail.com>
2020-07-24 14:28:17 -0700

    init

---
+++ BlazorMovie/BlazorMovie/Shared/Entities/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMovie.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
