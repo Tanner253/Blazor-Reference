9bbcc68dc52244db51519e264d71b4c30958bbed
Tanner Percival <percivaltanner@gmail.com>
2020-08-07 11:39:25 -0700

    invoke js function from file

--- BlazorMovie/BlazorMovie/Client/Helpers/IJSRuntimeExtensionMethods.cs
+++ BlazorMovie/BlazorMovie/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -10,7 +10,12 @@ namespace BlazorMovie.Client.Helpers
     {
         public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
         {
+            await js.InvokeVoidAsync("console.log", "Example Message");
             return await js.Confirm(message);
+        }
+        public static async ValueTask MyFunction(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("my_function", message);
         }
     }
 }
